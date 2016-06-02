f37e9fc7180c4f3f00ccc2aaeed0df5177b3aaf5
dacaiguoguo <dacaiguoguo@gmail.com>
2016-06-02 13:58:02 +0800

    move JSPatch.js to JSPatch_JS
    add
    JSPatch_JS.h
    JSPatch_JS.m

---
+++ JSPatch/JSPatch_JS.h
@@ -0,0 +1,10 @@
+//  JSPatch_JS.h
+//  JSPatch
+//
+//  Created by dacaiguoguo on 16/6/2.
+//  Copyright (c) 2016 dacaiguoguo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString * JSPatch_js();
